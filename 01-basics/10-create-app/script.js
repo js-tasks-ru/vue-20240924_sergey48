7c33e6ed8748501ad6b8c7c9c6310466325f18ca
sl <s.lenshin@ya.ru>
2024-10-01 18:13:10 +0300

    Решение задачи createApp: добавлено и смонтировано простейшее приложение, добавлен шаблон и метод форматирования для вывода текущей даты в определённом виде

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,24 @@
 import { defineComponent, createApp } from 'vue'
+
+
+const appConfig = defineComponent({
+  name: '01Basics',
+
+  setup: () => {
+
+    function formatAsLocalLongDate(date) {
+      return new Date(date).toLocaleDateString(navigator.language, {
+        dateStyle: 'long'
+      })
+    }
+    return {
+      formatAsLocalLongDate
+    }
+  },
+
+  template: '<div>Сегодня {{ formatAsLocalLongDate(new Date()) }}</div>>',
+})
+
+const app = createApp(appConfig)
+
+app.mount('#app');
