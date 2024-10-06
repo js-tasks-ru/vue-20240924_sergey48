1d967de92084a832030b629a829af9502023677e
sl <s.lenshin@ya.ru>
2024-10-06 18:01:15 +0300

    Решение задачи UiCounter

--- 03-components/40-UiCounter/UiCounter.js
+++ 03-components/40-UiCounter/UiCounter.js
@@ -9,15 +9,54 @@ export default defineComponent({
     UiButton,
   },
 
-  setup() {
-    // Рекомендуется для практики реализовать обработку событий внутри setup, а не непосредственно в шаблоне
+  props: {
+    count: {
+      type: Number,
+      required: true,
+    },
+
+    min: {
+      type: Number,
+      default: 0,
+    },
+
+    max: {
+      type: Number,
+      default: Infinity,
+    },
+  },
+
+  emits: ['update:count'],
+
+  setup(props, ctx) {
+    function decrementCount() {
+      if (props.count > props.min)
+        ctx.emit('update:count', props.count - 1)
+    }
+    function incrementCount() {
+      if (props.count < props.max)
+        ctx.emit('update:count', props.count + 1)
+    }
+
+    return {
+      decrementCount,
+      incrementCount,
+    }
   },
 
   template: `
     <div class="counter">
-      <UiButton aria-label="Decrement" disabled>➖</UiButton>
-      <span class="count" data-testid="count">3</span>
-      <UiButton aria-label="Increment">➕</UiButton>
+      <UiButton
+        aria-label="Decrement"
+        @click="decrementCount"
+        :disabled="count <= min"
+      >➖</UiButton>
+      <span class="count" data-testid="count">{{ count }}</span>
+      <UiButton
+        aria-label="Increment"
+        @click="incrementCount"
+        :disabled="count >= max"
+      >➕</UiButton>
     </div>
   `,
 })
